bcbed66c806eb6e37b4069163d272ad21ea4f9c4
Erez Volk <erez@neuralight.ai>
2024-10-25 17:24:30 +0300

    Cosmetic linting

--- wp2tt/input.py
+++ wp2tt/input.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
 """Base class for input sources (document formats)."""
 from abc import abstractmethod
 from abc import ABC
 from os import PathLike
-
-from typing import Iterable
+import typing as t
 
 from wp2tt.format import ManualFormat
 from wp2tt.styles import DocumentProperties
@@ -13,8 +11,8 @@ from wp2tt.styles import DocumentProperties
 class IDocumentInput(ABC):
     """A document."""
 
-    def set_nth(self, nth: int):
-        """When supported, make all IDs globally unique"""
+    def set_nth(self, nth: int) -> None:
+        """When supported, make all IDs globally unique."""
         if nth > 1:
             print(f"{type(self).__name__}.set_nth(): Not implemented")
 
@@ -22,19 +20,19 @@ class IDocumentInput(ABC):
     @abstractmethod
     def properties(self) -> DocumentProperties:
         """A DocumentProperties object."""
-        raise NotImplementedError()
+        raise NotImplementedError
 
-    def styles_defined(self) -> Iterable[dict[str, str]]:
+    def styles_defined(self) -> t.Iterable[dict[str, str]]:
         """Yield a Style object kwargs for every style defined in the document."""
-        raise NotImplementedError()
+        raise NotImplementedError
 
-    def styles_in_use(self) -> Iterable[tuple[str, str | None]]:
+    def styles_in_use(self) -> t.Iterable[tuple[str, str | None]]:
         """Yield a pair (realm, wpid) for every style used in the document."""
-        raise NotImplementedError()
+        raise NotImplementedError
 
-    def paragraphs(self) -> Iterable["IDocumentParagraph | IDocumentTable"]:
-        """Yields an IDocumentParagraph object for each body paragraph."""
-        raise NotImplementedError()
+    def paragraphs(self) -> t.Iterable["IDocumentParagraph | IDocumentTable"]:
+        """Yield an IDocumentParagraph object for each body paragraph."""
+        raise NotImplementedError
 
 
 class IDocumentParagraph(ABC):
@@ -42,28 +40,28 @@ class IDocumentParagraph(ABC):
 
     @abstractmethod
     def style_wpid(self) -> str | None:
-        """Returns the wpid for this paragraph's style."""
-        raise NotImplementedError()
+        """Return the wpid for this paragraph's style."""
+        raise NotImplementedError
 
     def format(self) -> ManualFormat:
-        """Returns manual formatting on this paragraph."""
+        """Return manual formatting on this paragraph."""
         return ManualFormat.NORMAL
 
     def is_page_break(self) -> bool:
-        """True iff the paragraph is a page break."""
+        """Return True iff the paragraph is a page break."""
         return False
 
     @abstractmethod
-    def text(self) -> Iterable[str]:
-        """Yields strings of plain text."""
-        raise NotImplementedError()
+    def text(self) -> t.Iterable[str]:
+        """Yield strings of plain text."""
+        raise NotImplementedError
 
     @abstractmethod
-    def chunks(self) -> Iterable["IDocumentSpan | IDocumentImage | IDocumentFormula"]:
+    def chunks(self) -> t.Iterable["IDocumentSpan | IDocumentImage | IDocumentFormula"]:
         """Yield all elements in the paragraph."""
-        raise NotImplementedError()
+        raise NotImplementedError
 
-    def spans(self) -> Iterable["IDocumentSpan"]:
+    def spans(self) -> t.Iterable["IDocumentSpan"]:
         """Yield an IDocumentSpan per text span."""
         for chunk in self.chunks():
             if isinstance(chunk, IDocumentSpan):
@@ -74,118 +72,121 @@ class IDocumentSpan(ABC):
     """A span of characters inside a document."""
 
     def style_wpid(self) -> str | None:
-        """Returns the wpid for this span's style."""
+        """Return the wpid for this span's style."""
         return None
 
-    def footnotes(self) -> Iterable["IDocumentFootnote"]:
-        """Yields an IDocumentFootnote object for each footnote in this span."""
-        raise NotImplementedError()
+    def footnotes(self) -> t.Iterable["IDocumentFootnote"]:
+        """Yield an IDocumentFootnote object for each footnote in this span."""
+        raise NotImplementedError
 
-    def comments(self) -> Iterable["IDocumentComment"]:
-        """Yields an IDocumentComment object for each comment in this span."""
-        raise NotImplementedError()
+    def comments(self) -> t.Iterable["IDocumentComment"]:
+        """Yield an IDocumentComment object for each comment in this span."""
+        raise NotImplementedError
 
     def format(self) -> ManualFormat:
-        """Returns manual formatting on this span."""
+        """Return manual formatting on this span."""
         return ManualFormat.NORMAL
 
     @abstractmethod
-    def text(self) -> Iterable[str]:
-        """Yields strings of plain text."""
-        raise NotImplementedError()
+    def text(self) -> t.Iterable[str]:
+        """Yield strings of plain text."""
+        raise NotImplementedError
 
 
 class IDocumentImage(ABC):
     """An image inside a document."""
 
     def alt_text(self) -> str | None:
-        """Alternative text, if it exists"""
+        """Get alternative text, if it exists."""
         return None
 
     @abstractmethod
     def suffix(self) -> str:
         """Extension (e.g., ".jpeg")."""
-        raise NotImplementedError()
+        raise NotImplementedError
 
     @abstractmethod
     def save(self, path: PathLike) -> None:
-        """Writes to a file."""
-        raise NotImplementedError()
+        """Write to a file."""
+        raise NotImplementedError
 
 
 class IDocumentFormula(ABC):
     """A formula inside a document."""
+
     @abstractmethod
     def raw(self) -> bytes:
-        """Return the original formula"""
-        raise NotImplementedError()
+        """Return the original formula."""
+        raise NotImplementedError
 
     @abstractmethod
     def mathml(self) -> str:
         """Return formula as MathML."""
-        raise NotImplementedError()
+        raise NotImplementedError
 
 
 class IDocumentTable(ABC):
-    """A table inside a document"""
+    """A table inside a document."""
 
     def style_wpid(self) -> str | None:
-        """Returns the wpid for this table's style."""
+        """Return the wpid for this table's style."""
         return None
 
     def format(self) -> ManualFormat:
-        """Returns manual formatting on this paragraph."""
+        """Return manual formatting on this paragraph."""
         return ManualFormat.NORMAL
 
     @property
     @abstractmethod
     def shape(self) -> tuple[int, int]:
-        """(number of rows, number of columns)"""
+        """Pandas-like (number of rows, number of columns)."""
 
     @property
     def header_rows(self) -> int:
-        """Number of header rows"""
+        """Number of header rows."""
         return 0
 
     @abstractmethod
-    def rows(self) -> Iterable["IDocumentTableRow"]:
-        """Iterates the rows of the table"""
-        raise NotImplementedError()
+    def rows(self) -> t.Iterable["IDocumentTableRow"]:
+        """Iterate the rows of the table."""
+        raise NotImplementedError
 
 
 class IDocumentTableRow(ABC):
-    """A row in a table inside a document"""
+    """A row in a table inside a document."""
 
     @abstractmethod
-    def cells(self) -> Iterable["IDocumentTableCell"]:
-        """Iterates the cells in the row"""
-        raise NotImplementedError()
+    def cells(self) -> t.Iterable["IDocumentTableCell"]:
+        """Iterate the cells in the row."""
+        raise NotImplementedError
 
 
 class IDocumentTableCell(ABC):
-    """A cell in a table inside a document"""
+    """A cell in a table inside a document."""
 
     @property
     def shape(self) -> tuple[int, int]:
-        """Number of rows and columns this cell spans"""
+        """Number of rows and columns this cell spans."""
         return (1, 1)
 
     @abstractmethod
     def contents(self) -> IDocumentParagraph:
-        """Get the contents of this cell"""
+        """Get the contents of this cell."""
 
 
 class IDocumentFootnote(ABC):
     """A footnote."""
 
-    def paragraphs(self) -> Iterable[IDocumentParagraph]:
-        """Yields an IDocumentParagraph object for each footnote paragraph."""
-        raise NotImplementedError()
+    @abstractmethod
+    def paragraphs(self) -> t.Iterable[IDocumentParagraph]:
+        """Yield an IDocumentParagraph object for each footnote paragraph."""
+        raise NotImplementedError
 
 
 class IDocumentComment(ABC):
     """A comment (balloon)."""
 
-    def paragraphs(self) -> Iterable[IDocumentParagraph]:
-        """Yields an IDocumentParagraph object for each comment paragraph."""
-        raise NotImplementedError()
+    @abstractmethod
+    def paragraphs(self) -> t.Iterable[IDocumentParagraph]:
+        """Yield an IDocumentParagraph object for each comment paragraph."""
+        raise NotImplementedError
