c7394ddd34e0c397c7436ee5e7da7794407976ca
Erez Volk <erez.volk@gmail.com>
2026-05-16 17:47:03 +0300

    More ty

--- src/wp2tt/zip.py
+++ src/wp2tt/zip.py
@@ -7,7 +7,7 @@ from lxml import etree
 class ZipDocument(zipfile.ZipFile):
     """Base class for zipped-xml, like .docx and .odt."""
 
-    def load_xml(self, path_in_zip: str) -> etree._Entity | None:
+    def load_xml(self, path_in_zip: str) -> etree._Element | None:
         """Parse an XML file inside the zipped doc, return root node."""
         try:
             with self.open(path_in_zip) as fobj:
